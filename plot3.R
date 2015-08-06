a95b4c72fc0aabf66adf3a25f21aa954c637adc4
janusng2000@gmail.com <janusng2000@gmail.com>
2015-08-06 11:47:16 +0800

    plot 3 answer upload.

---
+++ plot3.R
@@ -0,0 +1,18 @@
+plot3 <- function("household_power_consumption.txt") {
+        power <- read.table("household_power_consumption.txt", header=T, sep=";")
+        power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+        df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+        
+        df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+        df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+        df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+        
+        df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+        
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        lines(df$timestamp,df$Sub_metering_2,col="red")
+        lines(df$timestamp,df$Sub_metering_3,col="blue")
+        legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+        dev.copy(png, file="plot3.png", width=480, height=480)
+        dev.off()
+        cat("plot3.png has been saved in", getwd())
